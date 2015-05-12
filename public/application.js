acc927be937618b3330ddd27a75713678ebeba3d
Cyberspace27 <ariel.villarreal26@gmail.com>
2015-05-12 15:49:43 -0600

    Se agrego el modulo ngRoute para el en rutamiento tambien se configuro el routing y un servicion para que angular no tenga que consultar al servidor por la authentication del usuario de esta manera se inyecta por medio de este servicio

--- public/application.js
+++ public/application.js
@@ -1,5 +1,11 @@
 var mainApplicationModuleName = 'time_tracking';
-var mainApplicationModule = angular.module(mainApplicationModuleName, ['example']);
+var mainApplicationModule = angular.module(mainApplicationModuleName, ['ngRoute','users','example']);
+
+mainApplicationModule.config(['$locationProvider' ,
+	function($locationProvider){
+		$locationProvider.hashPrefix('!')
+	}
+	]);
 
 angular.element(document).ready(function(){
 	angular.bootstrap(document, [mainApplicationModuleName]);
