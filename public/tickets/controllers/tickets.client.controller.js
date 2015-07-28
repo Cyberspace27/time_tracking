6f6471eead90b5a8961476cf121ba03aa84e234e
Cyberspace27 <ariel.villarreal26@gmail.com>
2015-07-28 00:32:29 -0600

    se agrega estilo a la aplicacion con boostrap tambien se agrega una tabla para esto se modifica la view create-ticket y su controlador

--- public/tickets/controllers/tickets.client.controller.js
+++ public/tickets/controllers/tickets.client.controller.js
@@ -116,33 +116,9 @@ $scope.gridOptions = {
 
    };
 }
-
-/*
-		//Creamos un cronometro para llevar el tiempo invertido en el ticket
-		$scope.cronometro = function(){
-			contador_s = 0;
-		contador_m = 0;
-		s= document.getElementById("segundos");
-		m= document.getElementById("minutos");
-
-	tiempo = setInterval(
-		function(){
-			if(contador_s==60){
-				contador_s=0;
-				contador_m++;
-			m.innerHTML = contador_m;
-			}
-			s.innerHTML = contador_s;
-			contador_s++;
-	}
-	,1000);
-	   };
-
-	   $scope.detenerse= function(){
-		clearInterval(tiempo);
-       }
-*/	
-
+		$scope.tabla = function(){
+			
+		}
 
 	}
 	]);
