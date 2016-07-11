8fffbe95ea5ae8c44872323af0bd39803ab0bcfb
Cyberspace27 <ariel.villarreal26@gmail.com>
2016-07-11 15:11:02 -0600

    se crea el archivo productionjs

---
+++ config/env/production.js
@@ -0,0 +1,4 @@
+module.exports = {
+	db: 'mongodb://cyberspace27:1712aajj@ds027628.mlab.com:27628/time_tracking',
+	sessionSecret: 'developmentSessionSecret'
+};
--- config/mongoose.js
+++ config/mongoose.js
@@ -4,14 +4,21 @@
 //Carga las dependencias del modulo
 var config = require('./config'),
 	mongoose = require('mongoose');
+var env =  process.env.NODE_ENV = process.env.NODE_ENV || 'development'; 
 //Defenir el metodo de configuracion de mongoose
 module.exports = function(){
 	//Usar mongoose para conectar a MongoDB
 	
+
 	//Esta es la linea original
 	//var db = mongoose.connect(config.db);
-    //linea sustituta
-    var db = mongoose.connect('mongodb://cyberspace27:1712aajj@ds027628.mlab.com:27628/time_tracking');
+    //lineas sustituta
+    if ( env ==='development') {
+    	var db = mongoose.connect(config.db);
+   }else{
+    	var db = mongoose.connect('mongodb://cyberspace27:1712aajj@ds027628.mlab.com:27628/time_tracking');
+    };
+    
 	//Cargar el modelo "User"
 	require('../app/models/user.server.model');
 	//Cargar el modelo "Tickets"
